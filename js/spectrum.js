0d200e31f9a2716555de1d9b0f9cefe5f28fc57c
Hyuk Lee <deepsix2757@gmail.com>
2023-05-10 22:37:31 +0900

    add spectrum source

---
+++ js/spectrum.js
@@ -0,0 +1,16 @@
+import AudioMotionAnalyzer from 'https://cdn.skypack.dev/audiomotion-analyzer?min';
+
+// audio source
+const audioEl = document.getElementById('audio-controller');
+
+// instantiate analyzer
+const audioMotion = new AudioMotionAnalyzer(
+    document.getElementById('spectrum-analyzer'),
+    {
+        source: audioEl,
+        height: 240,
+        mode: 3,
+        barSpace: .4,
+        ledBars: true,
+    }
+);
